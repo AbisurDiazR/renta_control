dcf7964934b7dcb5ea0aaef8500dae9fd137f0b7
AbisurDiazR <abisurdiazramirez@gmail.com>
2025-03-25 17:20:31 -0600

    Sidebar menu

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,95 +1,16 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_side_menu/flutter_side_menu.dart';
-/*import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:renta_control/data/repositories/property_repository.dart';*/
-import 'package:renta_control/domain/models/nav_item_model.dart';
-/*import 'package:renta_control/presentation/blocs/properties/property_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:renta_control/data/repositories/property_repository.dart';
+import 'package:renta_control/presentation/blocs/properties/property_bloc.dart';
 import 'package:renta_control/presentation/blocs/properties/property_event.dart';
 import 'package:renta_control/presentation/blocs/properties/property_state.dart';
-import 'package:renta_control/presentation/pages/add_property_page.dart';*/
+import 'package:renta_control/presentation/pages/add_property_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  final _navAdmin = const [
-    NavItemModel(name: 'Prodiedades', icon: Icons.home),
-    NavItemModel(name: 'Usuarios', icon: Icons.person),
-    NavItemModel(name: 'Contratos', icon: Icons.assignment),
-    NavItemModel(name: 'Facturas', icon: Icons.receipt),
-    NavItemModel(name: 'Cerrar sesión', icon: Icons.exit_to_app)
-  ];
-
-  /*final _navUser = const [
-    NavItemModel(name: 'Prodiedades', icon: Icons.home),
-    NavItemModel(name: 'Usuarios', icon: Icons.person),
-    NavItemModel(name: 'Contratos', icon: Icons.assignment),
-    NavItemModel(name: 'Facturas', icon: Icons.receipt),
-    NavItemModel(name: 'Cerrar sesión', icon: Icons.exit_to_app)
-  ];*/
-
+  
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Panel de control',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          body: Row(
-            children: [
-              SideMenu(
-                mode: SideMenuMode.auto,
-                builder: (data) {
-                  return SideMenuData(
-                    header: Column(
-                      children: [
-                        ListTile(
-                          leading: Icon(Icons.arrow_right),
-                          title: Text('Control de rentas').showOrNull(data.isOpen),
-                          onTap: () {
-                            !data.isOpen;
-                          },
-                        ),
-                      ],
-                    ),
-                    items: [
-                      ..._navAdmin.map((e) => SideMenuItemDataTile(
-                        isSelected: e.name == 'Propiedades',
-                        onTap: () {},
-                        title: e.name,
-                        icon: Icon(
-                          e.icon,
-                          color: Color(0xff0055c3),
-                        ),
-                      )),
-                    ],
-                    footer: ListTile(
-                      title: Text('Foo Bar').showOrNull(data.isOpen),
-                      subtitle: Text('Foo Bar').showOrNull(data.isOpen),
-                      leading: Icon(Icons.person_pin),
-                    ),
-                  );
-                },
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.white,
-                  alignment: Alignment.center,
-                  child: Text('Body', style: Theme.of(context).textTheme.displaySmall,),
-                ),
-              )
-            ],
-          ),          
-        ),
-      ),
-    );
-  }
-
-  /*@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Lista de propiedades')),
@@ -171,10 +92,10 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
-  }*/
+  }
 }
 
-/*class SearchBar extends StatelessWidget {
+class SearchBar extends StatelessWidget {
   const SearchBar({super.key});
 
   @override
@@ -190,7 +111,4 @@ class HomePage extends StatelessWidget {
       },
     );
   }
-}*/
-extension on Widget{
-  Widget? showOrNull(bool isShow) => isShow ? this : null;
 }
